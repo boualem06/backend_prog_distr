a6ea88400fca2f3d4da0e489e29fc83fef953a17
boualem06 <kb_hamroune@esi.dz>
2025-02-25 20:05:47 +0100

    adding the deployment and the service é

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const Car = require('./models/Car');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 // Enable CORS for all routes
 app.use(cors());
